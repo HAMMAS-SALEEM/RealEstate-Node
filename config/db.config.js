c81d6cac7af30208c0c6d775c142a301691915ec
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-05-31 16:37:03 +0500

    Add Files

---
+++ config/db.config.js
@@ -0,0 +1,7 @@
+export const MONGO_URI = `mongodb+srv://hammassaleem376:<password>@cluster0.615ixwi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+export default {
+  HOST: '0.0.0.0',
+  PORT: 3001,
+  DB: 'realEstate'
+}
---
+++ models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const User = mongoose.model(
+    "User",
+    new mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    }),
+)
+
+export default User;
