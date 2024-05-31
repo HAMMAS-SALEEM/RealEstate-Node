7dc979e374a1b8188fb7967cc00057344589375e
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-05-31 18:05:37 +0500

    Update Authentication

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -1,6 +1,21 @@
 import jwt from 'jsonwebtoken'
-import authConfig from '../config/auth.config'
-import db from '../models'
+import config from '../config/auth.config'
 
-const User = db.user;
+verifyToken = (req, res, next) => {
+  let token = req.headers['x-access-token'];
+  if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+  jwt.verify(token,
+    config.secret,
+    (error, decoded) => {
+        if (error) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        req.userId = decoded.id;
+        next();
+    }
+  )
+}
 
+const authJwt = {
+  verifyToken,
+}
+
+export default authJwt;
