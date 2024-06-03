8047f33289ff64395005b92c8490472062e7cfb1
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-06-03 11:09:18 +0500

    Format Document

--- app/utils/tokenGenerator.js
+++ app/utils/tokenGenerator.js
@@ -1,10 +1,10 @@
 import jwt from 'jsonwebtoken'
 
 export default (id, secret) => {
-    const token = jwt.sign({ id }, secret, {
-        algorithm: 'HS256',
-        allowInsecureKeySizes: true,
-        expiresIn: 86400
-      })
-      return token;
+  const token = jwt.sign({ id }, secret, {
+    algorithm: 'HS256',
+    allowInsecureKeySizes: true,
+    expiresIn: 86400
+  })
+  return token
 }
