1776d65ac5a41c27082c6ab70f66932b4d39b405
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2024-06-07 21:19:49 +0500

    Handle Large File Size

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ export const MONGO_URI = `mongodb+srv://hammassaleem376:6oBlxFzkPWArOBFU@cluster
 const app = express()
 
 app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({limit: '10mb'}))
+app.use(express.urlencoded({ extended: true, limit: '10mb' }))
 
 mongoose
   .connect(MONGO_URI)
